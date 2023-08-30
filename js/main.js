697f3fce4c1d248f3c1e16b2a675914adb775d90
yudai19990419 <84347100+yudai19990419@users.noreply.github.com>
2023-08-30 21:57:23 +0900

    テキストフォントを50px設定

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 "use strict"
+const TEXT_FONT = "50px monospace";
 
 window.onload = function(){
     const canvas = document.getElementById("main");
     const context = canvas.getContext("2d");
+    context.font = TEXT_FONT;
     context.fillText("Hello world", 0, 64);
 }
