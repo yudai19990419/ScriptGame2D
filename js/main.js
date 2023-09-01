f961dfe72577041b2d8cec82b8acf8ec02f6376f
yudai19990419 <84347100+yudai19990419@users.noreply.github.com>
2023-09-01 23:24:02 +0900

    意図しないコードが入っていたため修正

--- js/main.js
+++ js/main.js
@@ -6,5 +6,5 @@ window.onload = function(){
     const context = canvas.getContext("2d");
     context.font = TEXT_FONT;
     context.fillText("Hello world", 0, 64);
-    context.fillText("update file", 20, 100);a59f694ad6dd6973c4d6367f24182af36d856
+    context.fillText("update file", 20, 100);
 }
